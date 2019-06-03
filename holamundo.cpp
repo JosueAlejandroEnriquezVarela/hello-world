3d2896e0c41d4b7116d562212bbb3ca9ce96ef10
JosueAlejandroEnriquezVarela <51309318+JosueAlejandroEnriquezVarela@users.noreply.github.com>
2019-06-03 01:33:43 -0500

    Add files via upload

---
+++ holamundo.cpp
@@ -0,0 +1,9 @@
+// holaMundo.cpp
+#include <iostream>
+// función principal
+int main()
+{
+    std::cout <<"Hola mundo en C++!\n";
+    std::cout<<std::endl;
+    return 0; //termina sin errores   
+} //Fin de función main 
